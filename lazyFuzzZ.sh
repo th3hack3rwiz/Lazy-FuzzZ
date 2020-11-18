999697407e0a2f6f9c3a63eac53df95995606537
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2020-11-18 13:51:12 +0530

    Update lazyFuzzZ.sh

--- lazyFuzzZ.sh
+++ lazyFuzzZ.sh
@@ -54,9 +54,9 @@ then usage
 		do echo -e "${CYAN}[+]Running on $line"
 		subdomain=$(echo ${line} | sed s/FUZZ//g | awk -F '/' '{print $3}')  #storing subdomain name
 		if [[ $new -eq 1 ]] ; then
-			 ffuf $(echo $flags) -u $line -w ${3} -of csv -o test > /dev/null
+			 ffuf $(echo $flags) -u $line -w ${3} -of csv -o test #> /dev/null
 		else
-			 ffuf -mc 200,403 -fs 0 -t 80 -sa -timeout 7 -u $line -w ${3} -of csv -o test > /dev/null
+			 ffuf -mc 200,403 -fs 0 -t 80 -sa -timeout 7 -u $line -w ${3} -of csv -o test #> /dev/null
 		fi
 		cat test | sed s/'^.*http'/http/g | sed 's/\,\,/ /g' | sed 's/ [[:digit:]]*,/                    /g' | sed 's/,$//g' | grep http > lazyFuzzZ.output.${3}/${subdomain}.output
 		if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]]	#checking if file is non empty
@@ -73,18 +73,24 @@ then usage
 				line_of_result=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | wc -l)
 				max_freq_size=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $3}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $2}')
 				max_size_freq=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $3}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $1}')
-				if [[ line_of_result -gt 1 ]]; then
+				if [[ line_of_result -gt 2 ]]; then
 					if [[ max_size_freq -le line_of_result/2 ]] ; then
 						echo -e "${GREEN}[+] Results were obtained for $line ! :D \n"
 					else
 						echo -e "${ORANGE}[+]More false positives detected! :-]] Removing them..."
 						cat lazyFuzzZ.output.${3}/${subdomain}.output | grep -v $max_freq_size > buff ; cat buff > lazyFuzzZ.output.${3}/${subdomain}.output ; rm buff
-						echo -e "${GREEN}[+] Results were obtained for $line ! :D \n"
+						if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]] ; then
+							echo -e "${GREEN}[+] Results were obtained for $line ! :D \n"
+						else
+							echo -e "${BLUE}[-] Results found were all false positives! :( Moving on..\n"
+							rm lazyFuzzZ.output.${3}/${subdomain}.output			#removing it if it's empty
+						fi	
 					fi 
 				else
+					cat lazyFuzzZ.output.${3}/${subdomain}.output | cut -d " " -f1 >> lazyFuzzZ.output.${3}/burpSeeds
 					echo -e "${GREEN}[+] Results were obtained for $line ! :D \n"
 				fi	
-					cat lazyFuzzZ.output.${3}/${subdomain}.output | cut -d " " -f1 >> lazyFuzzZ.output.${3}/burpSeeds
+					
 			else
 				echo -e "${BLUE}[-] Results found were all false positives! :( Moving on..\n"
 				rm lazyFuzzZ.output.${3}/${subdomain}.output			#removing it if it's empty
@@ -97,7 +103,7 @@ then usage
 		done
 		if [[ $dis -eq 0 ]] ; then
 			echo -e "\n${CYAN}[+]Firing up BurpFeed and sending the results to Burpsuite!"
-			python <path to bfeed.py>/bfeed.py lazyFuzzZ.output.${3}/burpSeeds > /dev/null
+			#python <path to bfeed.py>/bfeed.py lazyFuzzZ.output.${3}/burpSeeds > /dev/null
 		fi
 		echo -e "${GREEN}[+] Thank you for using Lazy FuzzZ! :D"
 	fi
