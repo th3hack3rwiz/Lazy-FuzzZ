8a1d01b64bbecf55bd8267df5a5082e1fcabd19b
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2020-11-18 20:38:08 +0530

    Update lazyFuzzZ.sh

--- lazyFuzzZ.sh
+++ lazyFuzzZ.sh
@@ -42,62 +42,61 @@ done
 shift $((OPTIND-1))
 
 
-if [[ $# -ne 3 ]]
-then usage
+if [[ $# -ne 3 ]] ; then
+	usage
 	echo -e "\n[+]Check usage!"
-	 else
-	 	printf "\n"
-		cat ${2} | grep ${1} | uniq | sed 's/'${1}'/'${1}'\/FUZZ/g' > ${1}.fuZZmeePleasee
-		echo -e "${GREEN}[+] Starting Lazy FuzzZ! :D\n"
-		mkdir lazyFuzzZ.output.${3}
-		for line in $(cat ${1}.fuZZmeePleasee) 
-		do echo -e "${CYAN}[+]Running on $line"
+else
+	printf "\n"
+	cat ${2} | grep ${1} | uniq | sed 's/'${1}'/'${1}'\/FUZZ/g' > ${1}.fuZZmeePleasee
+	echo -e "${GREEN}[+] Starting Lazy FuzzZ! :D\n"
+	mkdir lazyFuzzZ.output.${3}
+	for line in $(cat ${1}.fuZZmeePleasee) ; do
+		echo -e "${CYAN}[+]Running on $line"
 		subdomain=$(echo ${line} | sed s/FUZZ//g | awk -F '/' '{print $3}')  #storing subdomain name
 		if [[ $new -eq 1 ]] ; then
-			 ffuf $(echo $flags) -u $line -w ${3} -of csv -o test #> /dev/null
+			 ffuf $(echo $flags) -u $line -w ${3} -of csv -o test > /dev/null
 		else
-			 ffuf -mc 200,403 -fs 0 -t 80 -sa -timeout 7 -u $line -w ${3} -of csv -o test #> /dev/null
+			 ffuf -mc 200,403 -fs 0 -t 80 -sa -timeout 7 -u $line -w ${3} -of csv -o test > /dev/null
 		fi
 		cat test | sed s/'^.*http'/http/g | sed 's/\,\,/ /g' | sed 's/ [[:digit:]]*,/                    /g' | sed 's/,$//g' | grep http > lazyFuzzZ.output.${3}/${subdomain}.output
-		if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]]	#checking if file is non empty
-			then 
-				max_occurence=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $3}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $1}')
-				max_size=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $3}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $2}')
+		if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]] ; then 	#checking if file is non empty
+			max_occurence=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $2}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $1}')
+			max_size=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $2}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $2}')
 			if [[ max_occurence -gt 100 ]] ; then
 				echo -e "${MAGENTA}[+]Results obtained with false positives... Removing them..."
-				cat lazyFuzzZ.output.${3}/${subdomain}.output | grep -v $max_size > buff ; cat buff > lazyFuzzZ.output.${3}/${subdomain}.output
-			fi
-			if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]]	#checking if file is non empty
-
-				then
-				line_of_result=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | wc -l)
-				max_freq_size=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $3}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $2}')
-				max_size_freq=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $3}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $1}')
-				if [[ line_of_result -gt 2 ]]; then
-					if [[ max_size_freq -le line_of_result/2 ]] ; then
-						echo -e "${GREEN}[+] Results were obtained for $line ! :D \n"
-					else
-						echo -e "${ORANGE}[+]More false positives detected! :-]] Removing them..."
-						cat lazyFuzzZ.output.${3}/${subdomain}.output | grep -v $max_freq_size > buff ; cat buff > lazyFuzzZ.output.${3}/${subdomain}.output ; rm buff
-						if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]] ; then
-							echo -e "${GREEN}[+] Results were obtained for $line ! :D \n"
+				cat lazyFuzzZ.output.${3}/${subdomain}.output | grep -v $max_size > buff ; cat buff > lazyFuzzZ.output.${3}/${subdomain}.output ; rm buff
+				if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]] ;	then
+					line_of_result=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | wc -l)
+					max_freq_size=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $2}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $2}')
+					max_size_freq=$(cat lazyFuzzZ.output.${3}/${subdomain}.output | awk -F "," '{print $2}'| sort -n | grep [[:digit:]] | uniq -c | sort -k1 -nr | head -1 | awk '{print $1}')
+					if [[ $line_of_result -gt 2 ]]; then
+						if [[ $max_size_freq -le line_of_result/2 ]] ; then
+							echo -e "${GREEN}[+] $( cat lazyFuzzZ.output.${3}/${subdomain}.output | wc -l )  Results were obtained for $line ! :D \n"
 						else
-							echo -e "${BLUE}[-] Results found were all false positives! :( Moving on..\n"
-							rm lazyFuzzZ.output.${3}/${subdomain}.output			#removing it if it's empty
-						fi	
-					fi 
+							echo -e "${ORANGE}[+]More false positives detected! :-]] Removing them..."
+							cat lazyFuzzZ.output.${3}/${subdomain}.output | grep -v $max_freq_size > buff ; cat buff > lazyFuzzZ.output.${3}/${subdomain}.output ; rm buff
+							if [[ -s lazyFuzzZ.output.${3}/${subdomain}.output ]] ; then
+								echo -e "${GREEN}[+] $(cat lazyFuzzZ.output.${3}/${subdomain}.output | wc -l) Results were obtained for $line ! :D \n"
+							else
+								echo -e "${BLUE}[-] Results found were all false positives! :( Moving on..\n"
+								rm lazyFuzzZ.output.${3}/${subdomain}.output			#removing it if it's empty
+							fi	
+						fi 
+					else
+						cat lazyFuzzZ.output.${3}/${subdomain}.output | cut -d " " -f1 >> lazyFuzzZ.output.${3}/burpSeeds
+						echo -e "${GREEN}[+] $(cat lazyFuzzZ.output.${3}/${subdomain}.output | wc -l) Results were obtained for $line ! :D \n"
+					fi	
 				else
-					cat lazyFuzzZ.output.${3}/${subdomain}.output | cut -d " " -f1 >> lazyFuzzZ.output.${3}/burpSeeds
-					echo -e "${GREEN}[+] Results were obtained for $line ! :D \n"
-				fi	
-					
+					echo -e "${BLUE}[-] Results found were all false positives! :( Moving on..\n"
+					rm lazyFuzzZ.output.${3}/${subdomain}.output			#removing it if it's empty
+				fi				
 			else
-				echo -e "${BLUE}[-] Results found were all false positives! :( Moving on..\n"
-				rm lazyFuzzZ.output.${3}/${subdomain}.output			#removing it if it's empty
+				cat lazyFuzzZ.output.${3}/${subdomain}.output | cut -d " " -f1 >> lazyFuzzZ.output.${3}/burpSeeds
+				echo -e "${GREEN}[+] $(cat lazyFuzzZ.output.${3}/${subdomain}.output | wc -l) Results were obtained for $line ! :D \n"
 			fi
 		else
 			echo -e "${RED}[-]No results found! :-| Moving on..\n"
-			rm lazyFuzzZ.output.${3}/${subdomain}.output			#removing it if it's empty
+			rm lazyFuzzZ.output.${3}/${subdomain}.output
 		fi
 		sleep 7
 		done
@@ -106,4 +105,5 @@ then usage
 			#python <path to bfeed.py>/bfeed.py lazyFuzzZ.output.${3}/burpSeeds > /dev/null
 		fi
 		echo -e "${GREEN}[+] Thank you for using Lazy FuzzZ! :D"
+		rm test
 	fi
